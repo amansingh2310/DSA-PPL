568d00fc6cfd166125711d6fa4f6033e79470f7b
Aman Singh <singh.amankr23@gmail.com>
2024-02-18 21:12:28 +0530

    Loops

--- Begin with Programming/Loops/sum_of_terms_1.cpp
+++ Begin with Programming/Loops/sum_of_terms_1.cpp
@@ -4,17 +4,23 @@ using namespace std;
 
 int main() 
 {
-    int x, n;
-    cin>>x>>n;
-    float fact = 1.0, sum = 0.0;
-    int i = 1;
-    while(i <= n) {
-      fact *= i;
-      i++;
-    }
-    for(i = 0; i<n; i++) {
-      sum =  sum + (pow(x,i)/fact);
+    int n, x;
+    cin>>n>>x;
+    
+    double sum = 0.0;
+    for(int i=0; i<n; i++) {
+      int num;
+      for(int j=0; j<=i; j++){
+        num = pow(x,j);
+      }
+      double fact = 1.0;
+      for(int j=1; j<=i; j++){
+        fact *= j;
+      }
+      
+      sum = sum + (num/fact);
     }
     cout<<sum;
+
     return 0;
 }
