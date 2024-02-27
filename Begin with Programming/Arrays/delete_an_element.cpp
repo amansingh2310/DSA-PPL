a86d0a94f8f7003aebaf7cdd6ee8338a05112fcf
Aman Singh <singh.amankr23@gmail.com>
2024-02-27 21:21:23 +0530

    Arrays

--- Begin with Programming/Arrays/delete_an_element.cpp
+++ Begin with Programming/Arrays/delete_an_element.cpp
@@ -18,9 +18,14 @@ int main()
     int index;
     cin>>index;
     
-    for(int i=index; i<n; i++) {
-      arr[i] = arr[i+1];
+    for(int i=index+1; i<n; i++) {
+      arr[i-1] = arr[i];
     }
+    
+    // different condition
+    // for(int i=index; i<n; i++) {
+    //   arr[i] = arr[i+1];
+    // }
     
     for(int i=0; i<size; i++) {
       cout<<arr[i]<<" ";
