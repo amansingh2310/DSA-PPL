66f902be90c0dd90030f5026fb3196517f18e029
Aman Singh <singh.amankr23@gmail.com>
2024-02-27 21:12:09 +0530

    Arrays

---
+++ Begin with Programming/Arrays/delete_an_element.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int size;
+    cin>>size;
+    
+    int arr[size];
+    
+    int n;
+    cin>>n;
+    
+    for(int i=0; i<n; i++) {
+      cin>>arr[i];
+    }
+    
+    int index;
+    cin>>index;
+    
+    for(int i=index; i<n; i++) {
+      arr[i] = arr[i+1];
+    }
+    
+    for(int i=0; i<size; i++) {
+      cout<<arr[i]<<" ";
+    }
+    
+    return 0;
+}
