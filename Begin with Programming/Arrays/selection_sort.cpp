25ce8b43e76e6ae08b16deb5d64f42e36104acfd
Aman Singh <singh.amankr23@gmail.com>
2024-02-29 00:26:53 +0530

    Arrays

---
+++ Begin with Programming/Arrays/selection_sort.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int main() 
+{
+    int size;
+    cin>>size;
+    
+    int arr[size];
+    
+    for(int i=0; i<size; i++) {
+      cin>>arr[i];
+    }
+    
+    for(int i=0; i<size; i++) {
+      int min = INT_MAX, idx;
+      for(int j=i; j<size; j++) {
+        if(arr[j]<min) {
+          min = arr[j];
+          idx = j;
+        }
+      }
+      int temp = arr[i];
+      arr[i] = arr[idx];
+      arr[idx] = temp;
+    }
+    
+    for(int i=0; i<size; i++) {
+      cout<<arr[i]<<" ";
+    }
+    
+    return 0;
+}
