7ac2c5e46d7629dac0ab4c86afd8194de6635099
Aman Singh <singh.amankr23@gmail.com>
2024-03-04 00:48:38 +0530

    2D Arrays

--- Begin with Programming/Two Dimensional Arrays/adding_2_matrices.cpp
+++ Begin with Programming/Two Dimensional Arrays/adding_2_matrices.cpp
@@ -3,52 +3,49 @@ using namespace std;
 
 int main() 
 {
-    int m1, n1;
-    cin>>m1>>n1;
+    int m, n;
+    cin>>m>>n;
     
-    int arr1[m1][n1];
+    int arr1[m][n];
     
-    for(int i=0; i<m1; i++) {
-      for(int j=0; j<n1; j++) {
+    for(int i=0; i<m; i++) {
+      for(int j=0; j<n; j++) {
         cin>>arr1[i][j];
       }
     }
     
-    int m2, n2;
-    cin>>m2>>n2;
+    int arr2[m][n];
     
-    int arr2[m2][n2];
-    
-    for(int i=0; i<m2; i++) {
-      for(int j=0; j<n2; j++) {
+    for(int i=0; i<m; i++) {
+      for(int j=0; j<n; j++) {
         cin>>arr2[i][j];
       }
     }
     
-    int arr3[m1][n1];
+    int arr3[m][n];
     
-    for(int i=0; i<m1; i++) {
-      for(int j=0; j<n1; j++) {
+    for(int i=0; i<m; i++) {
+      for(int j=0; j<n; j++) {
         arr3[i][j] = arr1[i][j] + arr2[i][j];
       }
     }
     
-    for(int i=0; i<m1; i++) {
-      for(int j=0; j<n1; j++) {
+    for(int i=0; i<m; i++) {
+      for(int j=0; j<n; j++) {
         cout<<arr3[i][j]<<" ";
       }
       cout<<endl;
     }
     
-    // for(int i=0; i<m1; i++) {
-    //   for(int j=0; j<n1; j++) {
+    // for(int i=0; i<m; i++) {
+    //   for(int j=0; j<n; j++) {
     //     cout<<arr1[i][j]<<" ";
     //   }
     //   cout<<endl;
     // }
     
-    // for(int i=0; i<m1; i++) {
-    //   for(int j=0; j<n1; j++) {
+    // for(int i=0; i<m; i++) {
+    //   for(int j=0; j<n; j++) {
     //     cout<<arr2[i][j]<<" ";
     //   }
     //   cout<<endl;
