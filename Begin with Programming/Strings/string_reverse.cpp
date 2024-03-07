85a543d666450cce405e88602b63dd2ce653f6a6
Aman Singh <singh.amankr23@gmail.com>
2024-03-07 23:16:55 +0530

    Strings

--- Begin with Programming/Strings/string_reverse.cpp
+++ Begin with Programming/Strings/string_reverse.cpp
@@ -4,15 +4,24 @@ using namespace std;
 int main() 
 {
     char word[101] = "My name is Aman.";
+    char vowel[101] = "AEIOUaeiou";
+    int v_count = 0;
     
-    int c_word = 0;
     for(int i=0; word[i] != '\0'; i++) {
-      c_word++;
+      for(int j=0; vowel[j] != '\0'; j++) {
+        if(word[i] == vowel[j]) {
+          v_count++;
+        }
+      }
     }
     
-    for(int i=c_word-1; i>=0; i-- ) {
-      cout<<word[i];
-    }
+    // for(int i=0; word[i] != '\0'; i++) {
+    //   if(word[i] == 'A' || word[i] == 'E' || word[i] == 'I' || word[i] == 'O' || word[i] == 'U' || word[i] == 'a' || word[i] == 'e' || word[i] == 'i' || word[i] == 'o' || word[i] == 'u') {
+    //     v_count++;
+    //   }
+    // }
+    
+    cout<<v_count;
     
     return 0;
 }
