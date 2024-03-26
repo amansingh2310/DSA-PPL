1037fa6ebe93150eb34773ef7bdcb8117ce5d4c9
Aman Singh <singh.amankr23@gmail.com>
2024-03-26 22:42:51 +0530

    Strings

--- Begin with Programming/Strings/vowel_count.cpp
+++ Begin with Programming/Strings/vowel_count.cpp
@@ -4,13 +4,23 @@ using namespace std;
 int main() 
 {
     char word[101] = "My name is Aman.";
-    
+    char vowel[101] = "AEIOUaeiou";
     int v_count = 0;
+    
     for(int i=0; word[i] != '\0'; i++) {
-      if(word[i] == 'A' || word[i] == 'E' || word[i] == 'I' || word[i] == 'O' || word[i] == 'U' || word[i] == 'a' || word[i] == 'e' || word[i] == 'i' || word[i] == 'o' || word[i] == 'u') {
-        v_count++;
+      for(int j=0; vowel[j] != '\0'; j++) {
+        if(word[i] == vowel[j]) {
+          v_count++;
+        }
       }
     }
+    
+    // for(int i=0; word[i] != '\0'; i++) {
+    //   if(word[i] == 'A' || word[i] == 'E' || word[i] == 'I' || word[i] == 'O' || word[i] == 'U' || word[i] == 'a' || word[i] == 'e' || word[i] == 'i' || word[i] == 'o' || word[i] == 'u') {
+    //     v_count++;
+    //   }
+    // }
+    
     cout<<v_count;
     
     return 0;
