01ea9a0d5c9243af2a4b2081be7703f9365fe8ef
Aman Singh <singh.amankr23@gmail.com>
2024-03-27 23:16:58 +0530

    Arrays

--- Begin with Programming/Arrays/bubble_sort.cpp
+++ Begin with Programming/Arrays/bubble_sort.cpp
@@ -13,7 +13,7 @@ int main()
     }
     
     for(int i=0; i<size-1; i++) {
-      for(int j=0; j<size-1; j++) {
+      for(int j=0; j<size-i-1; j++) {
         if(arr[j] > arr[j+1]) {
           int temp = arr[j];
           arr[j] = arr[j+1];
---
+++ Begin with Programming/Strings/sort_string.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    char word[100] = "fsNHFOnlndFf";
+    
+    int w_len=0;
+    for(int i=0; word[i] != '\0'; i++) {
+      w_len++;
+    }
+    
+    // cout<<w_len<<endl;
+    
+    for(int j=0; j<w_len-1; j++) {
+      for(int i=0; i<w_len-j-1; i++) {
+        if(word[i] > word[i+1]) {
+          char temp = word[i];
+          word[i] = word[i+1];
+          word[i+1] = temp;
+        }
+      }
+    }
+    
+    cout<<word;
+    
+    return 0;
+
+
+
+
+
+
+}
